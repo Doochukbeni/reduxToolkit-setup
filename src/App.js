ac070609e4cbbf1d455b1f8608629f9e98e64deb
Doochukbeni <98967135+Doochukbeni@users.noreply.github.com>
2022-07-05 13:19:58 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from './components/NavBar';
+import CartContainer from './components/CartContainer';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <CartContainer />
+    </>
+  );
+}
+export default App;
